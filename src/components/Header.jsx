97100aad16eac115141fd54bf6418eb2f89ca62f
stonefulstm <stoneful.stm@gmail.com>
2022-08-01 22:08:36 -0300

    Requisito 4 - Criar links de navegação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -29,8 +30,16 @@ export default class Header extends Component {
         {
           !loadingAPI ? (
             <div>
-              <h1>TrybeTunes</h1>
-              <span data-testid="header-user-name">{ userName }</span>
+              <div className="titulo">
+                <h1>TrybeTunes</h1>
+                <span data-testid="header-user-name">{ userName }</span>
+              </div>
+              <nav>
+                <Link data-testid="link-to-search" to="/search">Search</Link>
+                <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+                <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+
+              </nav>
             </div>
           ) : (
             <div>
--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Loading extends Component {
   render() {
     return (
-      <h2>Carregando...</h2>
+      <h2 className="loading">Carregando...</h2>
     );
   }
 }
