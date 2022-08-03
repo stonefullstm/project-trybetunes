e604f6978a695074b05dd4999e1b6afcea6b0bfc
stonefulstm <stoneful.stm@gmail.com>
2022-08-03 10:15:13 -0300

    Requisito 7 - Listar músicas do almbum selecionado

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  state = {
+    album: [],
+  };
+
+  async componentDidMount() {
+    const {
+      match: { params: { id } },
+    } = this.props;
+    const albumDetails = await getMusics(id);
+    this.setState({
+      album: [...albumDetails],
+    });
+  }
+
   render() {
+    // const { match: { params: id } } = this.props;
+    const { album } = this.state;
+    const tracksList = album.filter((music) => music.kind === 'song');
+    const musicsList = tracksList.map((track) => (<MusicCard
+      key={ track.trackId }
+      trackName={ track.trackName }
+      previewUrl={ track.previewUrl }
+    />));
     return (
       <div data-testid="page-album">
-        Album
         <Header />
+        { album.length > 0 && (
+          <section>
+            <p data-testid="artist-name">{album[0].artistName}</p>
+            <p data-testid="album-name">{album[0].collectionName}</p>
+            <div>
+              { musicsList }
+            </div>
+          </section>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
